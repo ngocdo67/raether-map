1696d3ffd27995a4367f33f61100ed83aaf8368b
ngocdo67 <dohongngoc67@gmail.com>
2017-10-31 17:40:20 -0400

    added picasso jar

--- code/RaetherMap/app/src/main/java/trincoll/norahdo/raethermap/MainActivity.java
+++ code/RaetherMap/app/src/main/java/trincoll/norahdo/raethermap/MainActivity.java
@@ -1,43 +1,83 @@
 package trincoll.norahdo.raethermap;
 
+import android.app.DownloadManager;
 import android.app.SearchManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.location.LocationListener;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
 import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Raether Map";
-    private final int CODE_PERMISSIONS = 0;
+    private static final int CODE_PERMISSIONS = 1;
     private IALocationManager mIALocationManager;
+    private IAResourceManager mResourceManager;
+    private ImageView mFloorPlanImage;
+    private IAFloorPlan mFloorPlan;
+    private IATask<IAFloorPlan> mPendingAsyncResult;
+    private long mDownloadId;
+    private DownloadManager mDownloadManager;
+    private IAResourceManager mFloorPlanManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        String[] neededPermissions = {
-                android.Manifest.permission.CHANGE_WIFI_STATE,
-                android.Manifest.permission.ACCESS_WIFI_STATE,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-        };
-        ActivityCompat.requestPermissions( this, neededPermissions, CODE_PERMISSIONS );
-
         mIALocationManager = IALocationManager.create(this);
+        mFloorPlanImage = (ImageView) findViewById(R.id.map_image);
+
         mIALocationManager.registerRegionListener(mRegionListener);
+
+        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mIALocationListener);
+        Log.d(TAG, "onCreate/ requestLocationUpdates() called");
+
+        mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        mIALocationManager = IALocationManager.create(this);
+        mFloorPlanManager = IAResourceManager.create(this);
+
+        /* optional setup of floor plan id
+           if setLocation is not called, then location manager tries to find
+           location automatically */
+        final String floorPlanId = "9d6e58b2-1853-4de0-a655-b7f3368ea492";
+        if (!TextUtils.isEmpty(floorPlanId)) {
+            Toast.makeText(MainActivity.this, "Not valid floor plan id", Toast.LENGTH_LONG).show();
+            final IALocation location = IALocation.from(IARegion.floorPlan(floorPlanId));
+            mIALocationManager.setLocation(location);
+        }
     }
 
     private IALocationListener mIALocationListener = new IALocationListener() {
@@ -60,40 +100,147 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mIALocationListener);
+        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mLocationListener);
+        mIALocationManager.registerRegionListener(mRegionListener);
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        Log.d(TAG, "onResume/ requestLocationUpdates() called");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIALocationManager.removeLocationUpdates(mLocationListener);
+        mIALocationManager.unregisterRegionListener(mRegionListener);
+        unregisterReceiver(onComplete);
+        Log.d(TAG, "onPause/ removing requestLocationUpdates()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        mIALocationManager.destroy();
+        Log.d(TAG, "onDestroy/ destroying mIALocationManager");
+
+        super.onDestroy();
+    }
+
+    private IALocationListener mLocationListener = new IALocationListenerSupport() {
+        @Override
+        public void onLocationChanged(IALocation location) {
+            Log.d(TAG, "location is: " + location.getLatitude() + "," + location.getLongitude());
+        }
+    };
+
     private IARegion.Listener mRegionListener = new IARegion.Listener() {
-        // when null, we are not on any mapped area
-        // this information can be used for indoor-outdoor detection
-        IARegion mCurrentFloorPlan = null;
 
         @Override
         public void onEnterRegion(IARegion region) {
             if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
-                // triggered when entering the mapped area of the given floor plan
-                Log.d(TAG, "Entered " + region.getName());
-                Log.d(TAG, "floor plan ID: " + region.getId());
-                mCurrentFloorPlan = region;
-            }
-            else if (region.getType() == IARegion.TYPE_VENUE) {
-                // triggerend when near a new location
-                Log.d(TAG, "Location changed to " + region.getId());
+                String id = region.getId();
+                Log.d(TAG, "floorPlan changed to " + id);
+                Toast.makeText(MainActivity.this, id, Toast.LENGTH_SHORT).show();
+                fetchFloorPlan(id);
             }
         }
 
         @Override
         public void onExitRegion(IARegion region) {
             // leaving a previously entered region
-            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
-                mCurrentFloorPlan = null;
-                // notice that a change of floor plan (e.g., floor change)
-                // is signaled by an exit-enter pair so ending up here
-                // does not yet mean that the device is outside any mapped area
+        }
+
+    };
+
+    /*  Broadcast receiver for floor plan image download */
+    private BroadcastReceiver onComplete = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
+            if (id != mDownloadId) {
+                Log.w(TAG, "Ignore unrelated download");
+                return;
             }
+            Log.w(TAG, "Image download completed");
+            Bundle extras = intent.getExtras();
+            DownloadManager.Query q = new DownloadManager.Query();
+            q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
+            Cursor c = mDownloadManager.query(q);
+
+            if (c.moveToFirst()) {
+                int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    // process download
+                    String filePath = c.getString(c.getColumnIndex(
+                            DownloadManager.COLUMN_LOCAL_FILENAME));
+                    showFloorPlanImage(filePath);
+                }
+            }
+            c.close();
         }
     };
 
+    private void showFloorPlanImage(String filePath) {
+        Log.w(TAG, "showFloorPlanImage: " + filePath);
+        Picasso.with(this)
+                .load(filePath)
+                .into(mFloorPlanImage);
+    }
+
+    /**
+     * Fetches floor plan data from IndoorAtlas server. Some room for cleaning up!!
+     */
+    private void fetchFloorPlan(String id) {
+        cancelPendingNetworkCalls();
+        final IATask<IAFloorPlan> asyncResult = mFloorPlanManager.fetchFloorPlanWithId(id);
+        mPendingAsyncResult = asyncResult;
+        if (mPendingAsyncResult != null) {
+            mPendingAsyncResult.setCallback(new IAResultCallback<IAFloorPlan>() {
+                @Override
+                public void onResult(IAResult<IAFloorPlan> result) {
+                    Log.d(TAG, "fetch floor plan result:" + result);
+                    if (result.isSuccess() && result.getResult() != null) {
+                        mFloorPlan = result.getResult();
+                        String fileName = mFloorPlan.getId() + ".img";
+                        String filePath = Environment.getExternalStorageDirectory() + "/"
+                                + Environment.DIRECTORY_DOWNLOADS + "/" + fileName;
+                        File file = new File(filePath);
+                        if (!file.exists()) {
+                            DownloadManager.Request request =
+                                    new DownloadManager.Request(Uri.parse(mFloorPlan.getUrl()));
+                            request.setDescription("IndoorAtlas floor plan");
+                            request.setTitle("Floor plan");
+                            // requires android 3.2 or later to compile
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.
+                                        Request.VISIBILITY_HIDDEN);
+                            }
+                            request.setDestinationInExternalPublicDir(Environment.
+                                    DIRECTORY_DOWNLOADS, fileName);
+
+                            mDownloadId = mDownloadManager.enqueue(request);
+                        } else {
+                            showFloorPlanImage(filePath);
+                        }
+                    } else {
+                        // do something with error
+                        if (!asyncResult.isCancelled()) {
+                            Toast.makeText(MainActivity.this,
+                                    (result.getError() != null
+                                            ? "error loading floor plan: " + result.getError()
+                                            : "access to floor plan denied"), Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                }
+            }, Looper.getMainLooper()); // deliver callbacks in main thread
+        }
+    }
+
+    private void cancelPendingNetworkCalls() {
+        if (mPendingAsyncResult != null && !mPendingAsyncResult.isCancelled()) {
+            mPendingAsyncResult.cancel();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -117,4 +264,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Handle if any of the permissions are denied, in grantResults
     }
+
+
 }
--- code/RaetherMap/app/src/main/java/trincoll/norahdo/raethermap/SearchResultsActivity.java
+++ code/RaetherMap/app/src/main/java/trincoll/norahdo/raethermap/SearchResultsActivity.java
@@ -5,15 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 /**
  * Created by ngocdo67 on 10/16/17.
  */
@@ -32,14 +23,5 @@ public class SearchResultsActivity extends AppCompatActivity {
     }
 
     private void handleIntent (Intent intent) {
-        DatabaseReference myRef;
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            myRef = FirebaseDatabase.getInstance().getReference();
-//            myRef.orderByChild('_searchLastName')
-//                    .startAt(query)
-//                    .endAt(query+"\uf8ff")
-//                    .once("value")
-        }
     }
 }
